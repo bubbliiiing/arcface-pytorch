5a04a3862ff8cb9df8a6e4190ddca5426d591ce1
Bubbliiiing <3323290568@qq.com>
2022-03-24 00:05:21 +0800

    update fps

--- arcface.py
+++ arcface.py
@@ -110,3 +110,30 @@ class Arcface(object):
         plt.text(-12, -12, 'Distance:%.3f' % l1, ha='center', va= 'bottom',fontsize=11)
         plt.show()
         return l1
+
+    def get_FPS(self, image, test_interval):
+        #---------------------------------------------------#
+        #   对图片进行不失真的resize
+        #---------------------------------------------------#
+        image_data  = resize_image(image, [self.input_shape[1], self.input_shape[0]], self.letterbox_image)
+        #---------------------------------------------------------#
+        #   归一化+添加上batch_size维度
+        #---------------------------------------------------------#
+        image_data  = torch.from_numpy(np.expand_dims(np.transpose(preprocess_input(np.array(image_1, np.float32)), (2, 0, 1)), 0))
+        with torch.no_grad():
+            #---------------------------------------------------#
+            #   图片传入网络进行预测
+            #---------------------------------------------------#
+            preds = self.net(image_data).cpu().numpy()
+            
+        import time
+        t1 = time.time()
+        for _ in range(test_interval):
+            with torch.no_grad():
+                #---------------------------------------------------#
+                #   图片传入网络进行预测
+                #---------------------------------------------------#
+                preds = self.net(image_data).cpu().numpy()
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
--- predict.py
+++ predict.py
@@ -4,20 +4,41 @@ from arcface import Arcface
 
 if __name__ == "__main__":
     model = Arcface()
-    while True:
-        image_1 = input('Input image_1 filename:')
-        try:
-            image_1 = Image.open(image_1)
-        except:
-            print('Image_1 Open Error! Try again!')
-            continue
-
-        image_2 = input('Input image_2 filename:')
-        try:
-            image_2 = Image.open(image_2)
-        except:
-            print('Image_2 Open Error! Try again!')
-            continue
         
-        probability = model.detect_image(image_1,image_2)
-        print(probability)
+    #----------------------------------------------------------------------------------------------------------#
+    #   mode用于指定测试的模式：
+    #   'predict'表示单张图片预测，如果想对预测过程进行修改，如保存图片，截取对象等，可以先看下方详细的注释
+    #   'fps'表示测试fps，使用的图片是img里面的street.jpg，详情查看下方注释。
+    #----------------------------------------------------------------------------------------------------------#
+    mode            = "fps"
+    #-------------------------------------------------------------------------#
+    #   test_interval   用于指定测量fps的时候，图片检测的次数
+    #                   理论上test_interval越大，fps越准确。
+    #   fps_test_image  fps测试图片
+    #-------------------------------------------------------------------------#
+    test_interval   = 100
+    fps_test_image  = 'img/1_001.jpg'
+    
+    if mode == "predict":
+        while True:
+            image_1 = input('Input image_1 filename:')
+            try:
+                image_1 = Image.open(image_1)
+            except:
+                print('Image_1 Open Error! Try again!')
+                continue
+
+            image_2 = input('Input image_2 filename:')
+            try:
+                image_2 = Image.open(image_2)
+            except:
+                print('Image_2 Open Error! Try again!')
+                continue
+            
+            probability = model.detect_image(image_1,image_2)
+            print(probability)
+
+    elif mode == "fps":
+        img = Image.open(fps_test_image)
+        tact_time = model.get_FPS(img, test_interval)
+        print(str(tact_time) + ' seconds, ' + str(1/tact_time) + 'FPS, @batch_size 1')
